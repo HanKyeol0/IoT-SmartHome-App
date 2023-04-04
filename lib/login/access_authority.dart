617bd719b0bc7392abaea6777b55305bb02d73eb
Hankyeol <hankyeol1119@gmail.com>
2023-04-05 01:19:16 +0900

    home commit

--- lib/login/access_authority.dart
+++ lib/login/access_authority.dart
@@ -15,19 +15,16 @@ class _AccessAuthorityState extends State<AccessAuthority> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-            color: const Color(0xFF3E3E3E),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
+            decoration: const BoxDecoration(
+              color: Colors.grey,
             ),
-            child: Column(
+            child: const Column(
               children: [
-                Container(
-                  child: const Text(
-                    '액세스 권한 설정',
-                    style: TextStyle(color: Colors.white),
-                  ),
+                Text(
+                  '액세스 권한 설정',
+                  style: TextStyle(color: Colors.white),
                 ),
-                const Text(
+                Text(
                   "공동 현관문 출입, 주차 위치 저장 기능 등 기타 저장 기능을 위한 블루투스 위치 정보의 액세스 권한 설정이 반드시 필요합니다.",
                   style: TextStyle(color: Colors.white),
                 ),
